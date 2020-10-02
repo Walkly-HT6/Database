b8593a8d2410b5258bbcdb5a3e71d93e065f067b
Lilly Lubenova <lilly.lubenova@gmail.com>
2020-10-02 15:12:26 +0200

    Database creation

---
+++ fill_categories.sql
@@ -0,0 +1,8 @@
+INSERT INTO categories (categories_name)
+VALUES ("Fashion");
+
+INSERT INTO categories (categories_name)
+VALUES ("Food");
+
+INSERT INTO categories (categories_name)
+VALUES ("Transport");
